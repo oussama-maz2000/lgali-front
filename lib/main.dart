4a522cfead2f8b566bd98cc425b6b20f65bb1364
OussamaMaz <70782166+oussama-maz2000@users.noreply.github.com>
2023-04-30 17:57:11 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,15 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/splash.view.dart';
 import 'model/repository/profileRepository.dart';
 
-
-
 ProfileRepository profileRepository = ProfileRepository();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('user');
-await Supabase.initialize(url: "https://gihgpgzukilittqwsqxv.supabase.co", anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpaGdwZ3p1a2lsaXR0cXdzcXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM1MjcxNDQsImV4cCI6MTk4OTEwMzE0NH0.qSwJKb8dBgDA8aWghwAslDXwf5wjjgzJANLlsdAiNdY");
-
+  await Supabase.initialize(
+      url: "https://gihgpgzukilittqwsqxv.supabase.co",
+      anonKey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpaGdwZ3p1a2lsaXR0cXdzcXh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM1MjcxNDQsImV4cCI6MTk4OTEwMzE0NH0.qSwJKb8dBgDA8aWghwAslDXwf5wjjgzJANLlsdAiNdY");
 
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
