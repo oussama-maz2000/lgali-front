00d2bd000a3d543fa4f23ca135b00c1e6799ed5e
oussama-maz2000 <loulououssama2015@gmail.com>
2023-05-18 15:03:00 +0100

    update logo app

--- lib/screens/splash.view.dart
+++ lib/screens/splash.view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lgali/controllers/login.controller.dart';
 import 'package:lgali/screens/Sign_up_test.dart';
+import 'package:lgali/screens/displayClientInformation.view.dart';
 import 'package:lgali/screens/home.view.dart';
 import 'package:lgali/screens/log_in_test.dart';
 import 'package:lgali/screens/login.view.dart';
@@ -23,60 +24,30 @@ import '../shared/loading.dart';
 import '../shared/tabBarTest.dart';
 import 'dashbord.view.dart';
 import 'displayRequest.view.dart';
-import 'display_request_companies.view.dart';
+import 'displayRequestCompanies.view.dart';
 import 'otp_test_view.dart';
 import 'stepper/company_info.view.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 4), () {
-      print('called user info screen');
-      Get.to(() => RegisterScreen());
-    });
+    Future.delayed(Duration(seconds: 2)).then((value) =>
+        {print('calling user info screen'), Get.to(() => DashBordScreen())});
     return Scaffold(
+        backgroundColor: GlobalColor.redColor,
         body: Center(
-      child: Stack(alignment: AlignmentDirectional.center, children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(color: GlobalColor.backGroundColor),
-        ),
-        Positioned(
-          top: MediaQuery.of(context).size.width / 1.3,
-          child: Column(
-            children: [
-              Container(
-                height: 120.0,
-                width: 220.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/splach.png'),
-                    fit: BoxFit.fill,
-                  ),
-                  shape: BoxShape.rectangle,
-                ),
+          child: Container(
+            height: 200.0,
+            width: 220.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage('assets/images/splach1.png'),
+                fit: BoxFit.fill,
               ),
-              SizedBox(
-                height: 9,
-              ),
-              Container(
-                child: Text(
-                  'El Gali',
-                  style: TextStyle(
-                    fontFamily: 'Oxygen',
-                    color: Color(0xFFEAEFF7),
-                    fontSize: 36,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 1.2,
-                  ),
-                ),
-              )
-            ],
+              shape: BoxShape.rectangle,
+            ),
           ),
-        )
-      ]),
-    ));
+        ));
   }
 }
