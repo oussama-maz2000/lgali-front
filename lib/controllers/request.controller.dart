95c9b643e6924042dc235bcd9516894d1c23731c
oussama-maz2000 <loulououssama2015@gmail.com>
2023-05-19 18:37:57 +0100

    request controller

--- lib/controllers/request.controller.dart
+++ lib/controllers/request.controller.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 
 import 'package:lgali/model/repository/requestRepository.dart';
 import 'package:lgali/shared/custom_snack_bar.dart';
@@ -10,6 +11,7 @@ import 'package:lgali/shared/global.color.dart';
 class RequestController extends GetxController {
   var repository = Get.lazyPut(() => RequestRepository());
   var _requestRepository = Get.find<RequestRepository>();
+  var box = Hive.box('user');
 
   var selectedItem = ''.obs;
 
@@ -31,12 +33,8 @@ class RequestController extends GetxController {
     if (selectedItem.value.isEmpty != true && index.value == 0) {
       index.value = 1;
       await Future.delayed(Duration(seconds: 4), () {
-        /*  _requestRepository.createNewRequest(
-            'eac22d54-635b-48d8-943c-77465cd136e4',
-            selectedItem.value,
-            descriptionController.text,
-            latitude.value,
-            longitude.value); */
+        _requestRepository.createNewRequest(box.get('id'), selectedItem.value,
+            descriptionController.text, latitude.value, longitude.value);
 
         index.value = 0;
 
