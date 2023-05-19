44f4793ad2c0eff761e0f194b64fa3a18f17d527
oussama-maz2000 <loulououssama2015@gmail.com>
2023-05-19 18:36:59 +0100

    accept request controller

--- lib/controllers/acceptRequest.controller.dart
+++ lib/controllers/acceptRequest.controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 import 'package:lgali/model/repository/companyRepository.dart';
 import 'package:lgali/model/repository/requestRepository.dart';
 import 'package:logging/logging.dart';
@@ -9,6 +10,7 @@ class AcceptController extends GetxController {
   RequestRepository repository = Get.put(RequestRepository(), permanent: false);
   CompanyRepository companyRepository =
       Get.put(CompanyRepository(), permanent: false);
+  var box = Hive.box('user');
 
   final supabase = Supabase.instance.client;
 
@@ -24,10 +26,9 @@ class AcceptController extends GetxController {
     final subscription = supabase
         .from('requests')
         .stream(primaryKey: ['user_id'])
-        .eq("user_id", "eac22d54-635b-48d8-943c-77465cd136e4")
+        .eq("user_id", box.get('id'))
         .listen((List<Map<String, dynamic>> event) async {
-          requests.value = await repository
-              .getAllRequests("eac22d54-635b-48d8-943c-77465cd136e4");
+          requests.value = await repository.getAllRequests(box.get('id'));
 
           requests.value.forEach((element) {
             if (element['cluster'] != -1)
